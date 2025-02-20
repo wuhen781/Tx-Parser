bc66fb5de552c764be2693015b6b40375ddbc4aa
wuhen781 <wuhen781@gmail.com>
2025-02-21 00:28:53 +0700

    add unit testing

--- internal/service/service.go
+++ internal/service/service.go
@@ -41,3 +41,56 @@ func (this *EthParser) GetTransactions(address string) []database.Transaction {
 	modelParser := model.NewModelParser()
 	return modelParser.GetTransactions(address)
 }
+
+//Update transactions regularly in the background
+func (this *EthParser) UpdateTransactionsInBackGroundRegularly(ctx context.Context, interval int) {
+	timer := time.NewTimer(0)
+	defer timer.Stop()
+
+	modelParser := model.NewModelParser()
+	client := ethclient.NewEthclient("")
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-timer.C:
+		}
+		start := time.Now()
+
+		lastBlockNumber := modelParser.db.GetLastUpdatedBlockNumber()
+
+		blockNumber, err := client.GetCurrentBlock()
+		transactions, err2 := client.GetBlockByNumber(lastBlockNumber)
+		if err != nil {
+			log.Printf("Error getting current block: %v", err)
+		} else if err2 != nil {
+			log.Printf("Error getting block by number %v", err)
+		} else {
+			dbTransactions := make([]database.Transaction, len(transactions))
+			for idx, transaction := range transactions {
+				dbTx := database.Transaction{
+					From:        transaction.From,
+					To:          transaction.To,
+					Value:       transaction.Value,
+					BlockNumber: transaction.BlockNumber,
+					Gas:         transaction.Gas,
+					GasPrice:    transaction.GasPrice,
+					Hash:        transaction.Hash,
+					Nonce:       transaction.Nonce,
+					Timestamp:   transaction.Timestamp,
+					Type:        "",
+				}
+				dbTransactions[idx] = dbTx
+			}
+			err3 := modelParser.updateTransactionsByLastBlockNumber(currentBlockNumber, dbTransactions)
+			if err3 != nil {
+				log.Printf("Error updateTransactionsByLastBlockNumber %v", err)
+			} else {
+				log.Printf("Debug updateTransactionsByLastBlockNumber lastBlockNumber = %d , currentBlockNumber, len(transactions) = %d", lastBlockNumber, currentBlockNumber, len(dbTransactions))
+			}
+		}
+		timer.Reset(interval)
+	}
+
+}
---
+++ internal/service/service_test.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestEthParser(t *testing.T) {
+	parser := NewEthParser()
+
+	// Test GetCurrentBlock
+	blockNumber, err := parser.GetCurrentBlock()
+	if err != nil {
+		t.Errorf("Error getting current block: %v", err)
+	}
+	if blockNumber < 0 {
+		t.Errorf("Expected valid block number, got %d", blockNumber)
+	}
+
+	// Test Subscribe
+	if !parser.Subscribe("0x123") {
+		t.Errorf("Failed to subscribe to address")
+	}
+
+	// Test GetTransactions
+	txs := parser.GetTransactions("0x123")
+	if len(txs) != 0 {
+		t.Errorf("Expected 0 transactions, got %d", len(txs))
+	}
+}
+
+func TestUpdateTransactionsInBackGroundRegularly(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	parser := NewEthParser()
+
+	// Run the update function in a separate goroutine
+	go func() {
+		parser.UpdateTransactionsInBackGroundRegularly(ctx, 1*time.Second)
+	}()
+
+	// Allow some time for execution
+	time.Sleep(3 * time.Second)
+
+	// Stop the background process
+	cancel()
+}
